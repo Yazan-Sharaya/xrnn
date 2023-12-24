b843107746bde397c1bfd82bd7d982d3e5c6e0ba
Yazan Sharaya <yazan.sharaya.yes@gmail.com>
2023-12-24 17:46:21 +0300

    Extended the test for `layer_memory_consumption` to cover all cases and added more test cases

--- tests/test_layer_utils.py
+++ tests/test_layer_utils.py
@@ -37,27 +37,36 @@ def test_padded_output_shape(input_shape, window_size, strides, expected, image_
 
 
 @pytest.mark.parametrize(
-    "layer, expected_params, expected_total",
+    "layer, expected_params, expected_activations",
     [
-        (layers.Dense(8), 8 * 4 + 8 * 256 * 4, dense_batch_nbytes + 8 * 4 + 8 * 256 * 4 + (8 * 4 + 8 * 256 * 4) * 3),
+        (layers.Dense(8), 8 * 4 + 8 * 256 * 4, dense_batch_nbytes),
         (layers.Dropout(0.5), 0, image_batch_nbytes * 2),
-        (layers.Conv2D(8, 3, 2), 896, image_batch_nbytes + 896 + 896 * 3),
-        # (layers.Conv2D(8, 3, 2, 'same'), 896, 128 * 33 * 33 * 3 * 4 + 896 + 896 * 3),
-        # (layers.MaxPooling2D(2, 1), 0, 0),
-        # (layers.MaxPooling2D(2, 1, 'same'), 0, 0),
-        # (layers.AvgPooling2D(2, 2), 0, 0),
-        # (layers.AvgPooling2D(2, 2, 'same'), 0, 0),
-        (layers.BatchNormalization(), 60, image_batch_nbytes * 3 + 60 + 24 * 3),
+        (layers.Conv2D(8, 3, 2), 896, image_batch_nbytes),
+        (layers.Conv2D(8, 3, 2, 'same'), 896, 128 * 33 * 33 * 3 * 4),
+        (layers.Conv2D(8, 5, 3, 'same'), 5 * 5 * 8 * 3 * 4 + 8 * 4, 128 * 35 * 35 * 3 * 4),
+        (layers.MaxPooling2D(2, 1), 0, image_batch_nbytes),
+        (layers.MaxPooling2D(2, 1, 'same'), 0, 128 * 33 * 33 * 3 * 4),
+        (layers.AvgPooling2D(2, 2), 0, image_batch_nbytes),
+        (layers.AvgPooling2D(2, 2, 'same'), 0, 128 * 32 * 32 * 3 * 4),
+        (layers.BatchNormalization(), 60, image_batch_nbytes * 3),
         (layers.Flatten(), 0, image_batch_nbytes)
     ]
 )
-def test_layer_memory_consumption(layer, expected_params, expected_total, request):
+def test_layer_memory_consumption(layer, expected_params, expected_activations, request):
+    # `request` is a `pytest` built-in function that is passed to tests as an argument.
     for input_shape, image_format, dtype in zip(
             (nhwc_shape, nchw_shape), ('channels_last', 'channels_first'), ('f', 'd')):
         config.set_image_data_format(image_format)
         config.set_default_dtype(dtype)
+        if isinstance(layer, layers.BatchNormalization):
+            grads_mem = nhwc_shape[-1] * 4 * 2
+            expected_total = expected_activations + expected_params + grads_mem * 3
+        else:
+            grads_mem = expected_params
+            expected_total = expected_activations + expected_params * 4
         if dtype == 'd':
             expected_params *= 2
+            grads_mem *= 2
             expected_total *= 2
         input_shape = dense_shape if isinstance(layer, layers.Dense) else input_shape
         if getattr(layer, 'built') is False:
@@ -66,6 +75,13 @@ def test_layer_memory_consumption(layer, expected_params, expected_total, reques
             request.getfixturevalue(layer) if isinstance(layer, str) else layer, input_shape, True, True)
         assert params_mem_consumption == expected_params
         assert total_mem_consumption == expected_total
+        params_mem_consumption, *_, total_mem_consumption = layer_utils.layer_memory_consumption(
+            request.getfixturevalue(layer) if isinstance(layer, str) else layer, input_shape, False, True)
+        assert params_mem_consumption == total_mem_consumption == expected_params
+        params_mem_consumption, *_, total_mem_consumption = layer_utils.layer_memory_consumption(
+            request.getfixturevalue(layer) if isinstance(layer, str) else layer, input_shape, True, False)
+        assert params_mem_consumption == expected_params
+        assert total_mem_consumption == expected_total - grads_mem
 
 
 def test_make_unique_name():
