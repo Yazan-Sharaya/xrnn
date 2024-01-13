688c473843712a512f31dd13e849132d2ba92d64
Yazan Sharaya <yazan.sharaya.yes@gmail.com>
2024-01-13 19:35:19 +0300

    Added tests for binary crossentropy and mse

--- tests/test_losses.py
+++ tests/test_losses.py
@@ -1,4 +1,5 @@
 import sys
+
 if sys.version_info.minor > 6:
     from contextlib import nullcontext
 else:
@@ -10,7 +11,7 @@ import pytest
 
 
 def one_hot_encoded(n_classes):
-    integers = ops.random.integers(0, n_classes, (64, ))
+    integers = ops.random.integers(0, n_classes, (64,))
     one_hot = ops.zeros((64, n_classes))
     one_hot[ops.arange(len(integers)), integers] = 1
     return one_hot
@@ -40,8 +41,7 @@ class TestLoss:
         # calculates the loss value correctly, that is tested for each loss function separately.
         loss = losses.BinaryCrossentropy()
         y_true = ops.random.integers(0, 2, (64, 1))
-        loss_value = loss.calculate(y_true, y_true)
-        assert loss_value == pytest.approx(0, 1.1e-7, 1.1e-7)
+        assert loss.calculate(y_true, y_true) == pytest.approx(0, 1.1e-7, 1.1e-7)
 
 
 class TestCategoricalCrossentropy:
@@ -50,9 +50,9 @@ class TestCategoricalCrossentropy:
         "y_true, raises",
         [
             (ops.random.integers(0, 10, (64, 1)), nullcontext()),
-            (ops.random.integers(0, 10, (64, )), nullcontext()),
+            (ops.random.integers(0, 10, (64,)), nullcontext()),
             (ops.random.integers(0, 30, (64, 1)), pytest.raises(IndexError)),
-            (ops.random.integers(0, 30, (64, )), pytest.raises(IndexError)),
+            (ops.random.integers(0, 30, (64,)), pytest.raises(IndexError)),
             (one_hot_encoded(10), nullcontext()),
             (one_hot_encoded(30), pytest.raises(IndexError))
         ]
@@ -68,7 +68,42 @@ class TestCategoricalCrossentropy:
             # We don't want to raise IndexError from the test itself so this avoids that when n_classes > n_neuron.
             y_true_clipped[y_true_clipped >= 10] = 9
             y_pred[ops.arange(len(y_true)), y_true_clipped] = 1  # To match y_pred to y_true to get zero loss.
-            loss_value = loss.calculate(y_true, y_pred)
-            assert loss_value == pytest.approx(0, 1.1e-7, 1.1e-7)  # y_pred values that are equal to
+            assert loss.calculate(y_true, y_pred) == pytest.approx(0, 1.1e-7, 1.1e-7)  # y_pred values that are equal to
             # zero or one are clipped to config.EPSILON (default is 1e-7) so even if y_true and y_pred exactly match
             # loss isn't going to be exactly zero.
+
+
+class TestBinaryCrossentropy:
+
+    @pytest.mark.parametrize(
+        "y_true",
+        [
+            (ops.random.integers(0, 2, (64, 1))),
+            (ops.random.integers(0, 2, (64, ))),
+            (one_hot_encoded(1)),
+            (one_hot_encoded(1)),
+        ]
+    )
+    def test_forward(self, y_true):
+        loss = losses.BinaryCrossentropy()
+        y_pred = ops.expand_dims(y_true, 1) if y_true.ndim == 1 else y_true
+        # y_pred is always going to be 2-dimensional coming out of the network so no need to do this in a real scenario.
+        assert loss.calculate(y_true, y_pred) == pytest.approx(0, 1.1e-7, 1.2e-7)
+
+
+class TestMSE:
+
+    @pytest.mark.parametrize(
+        "y_true",
+        [
+            (ops.random.random((64, 1))),
+            (ops.random.random((64, ))),
+            (ops.random.uniform(-1, 1, (64, 16, 16, 3))),
+            (ops.random.standard_normal((64, 2))),
+            (ops.random.integers(0, 2768, (128, 100))),
+        ]
+    )
+    def test_forward(self, y_true):
+        loss = losses.MSE()
+        y_pred = ops.expand_dims(y_true, 1) if y_true.ndim == 1 else y_true
+        assert loss.calculate(y_true, y_pred) == pytest.approx(0, 1.1e-7, 1.1e-7)
