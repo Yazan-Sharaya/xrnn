54983e06f4abd86a64425444f316c8081b27a17f
Yazan Sharaya <yazan.sharaya.yes@gmail.com>
2024-01-13 19:35:28 +0300

    Accepts string loss, implemented `__call__`

--- xrnn/metrics.py
+++ xrnn/metrics.py
@@ -1,16 +1,30 @@
 """Defines metric classes that can be used to calculate the model's performance against said metric."""
-from typing import Callable
+from typing import Callable, Literal, Union
 from xrnn import losses
 from xrnn import ops
 
 
 class Accuracy:
 
-    def __init__(self, loss: losses.Loss) -> None:
-        """Measures the model accumulated accuracy."""
+    def __init__(
+            self, loss: Union[Literal['mse', 'binary_crossentropy', 'categorical_crossentropy'], losses.Loss]) -> None:
+        """
+        Measures the model accumulated accuracy.
+
+        Parameters
+        ----------
+        loss: str, Loss
+            Loss function class or a string representing the loss function used, available options are 'mse',
+            'binary_crossentropy', 'categorical_crossentropy'.
+        """
         self.accumulated_acc = 0
         self.accumulated_count = 0
+        if isinstance(loss, str):
+            if loss not in ('mse', 'binary_crossentropy', 'categorical_crossentropy'):
+                raise ValueError('`loss` must be one of "mse", "binary_crossentropy", "categorical_crossentropy".')
+            loss = loss.lower()
         self.loss = loss
+        self.acc_function = self.get_comparison_function()
 
     def reset_count(self) -> None:
         """Resets the accumulated accuracy and step count to start over again, called at the start of each epoch."""
@@ -20,13 +34,13 @@ class Accuracy:
     def get_comparison_function(self) -> Callable:
         """Decides the function that calculates the accuracy based on the loss function and returns it."""
         # Categorical accuracy
-        if isinstance(self.loss, losses.CategoricalCrossentropy):
+        if isinstance(self.loss, losses.CategoricalCrossentropy) or self.loss == 'categorical_crossentropy':
             return lambda y_true, y_pred: ops.argmax(y_pred, axis=1) == y_true
         # Binary accuracy
-        if isinstance(self.loss, losses.BinaryCrossentropy):
+        if isinstance(self.loss, losses.BinaryCrossentropy) or self.loss == 'binary_crossentropy':
             return lambda y_true, y_pred: (y_pred > 0.5).astype(int) == y_true
         # Regression accuracy
-        if isinstance(self.loss, losses.MeanSquaredError):
+        if isinstance(self.loss, losses.MeanSquaredError) or self.loss == 'mse':
             return lambda y_true, y_pred: ops.absolute(y_true - y_pred) < (ops.std(y_true) / 250)
 
     def calculate(self, y_true: ops.ndarray, y_pred: ops.ndarray) -> float:
@@ -36,13 +50,17 @@ class Accuracy:
 
         Notes
         -----
-        This method calculates epoch accuracy and not step accuracy.
+        This method calculates accumulated accuracy and not step accuracy.
         """
-        acc_function = self.get_comparison_function()
-        if isinstance(self.loss, losses.CategoricalCrossentropy):
-            if len(y_true.shape) == 2:  # if labels are one-hot encoded, convert them to sparse.
+        if isinstance(self.loss, losses.CategoricalCrossentropy) or self.loss == 'categorical_crossentropy':
+            if y_true.ndim == 2:  # if labels are one-hot encoded, convert them to sparse.
                 y_true = ops.argmax(y_true, axis=1)
-        comparisons = acc_function(y_true, y_pred)
+        comparisons = self.acc_function(y_true, y_pred)
         self.accumulated_acc += ops.sum(comparisons)
         self.accumulated_count += len(comparisons)
         return self.accumulated_acc / self.accumulated_count
+
+    def __call__(self, y_true: ops.ndarray, y_pred: ops.ndarray) -> float:
+        """Same as `calculate`. Implemented just to have the same API as other classes in this package where you can get
+        the result by calling the object and not the `calculate/forward` method directly."""
+        return self.calculate(y_true, y_pred)
