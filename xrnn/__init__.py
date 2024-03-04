1b36445d54a746c63c805bd0a004b8a6c9581521
Yazan Sharaya <yazan.sharaya.yes@gmail.com>
2024-03-04 08:43:38 +0300

    Bump version from 1.0.2 to 1.1.0

--- xrnn/__init__.py
+++ xrnn/__init__.py
@@ -4,4 +4,5 @@
 * xrnn only requires one third library dependency which is NumPy (almost likely you had it already installed).
 * For more information, look at `layers.py` and `models.py` docstrings, and for a minimal working example of how to use
   the package to train a CNN on MNIST dataset, see `example.py`."""
-__version__ = "1.0.2"
+
+__version__ = "1.1.0"
