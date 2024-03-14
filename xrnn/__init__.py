92e33c4ba7baa82e3dd9fd4ea96057dc7f20880b
Yazan Sharaya <yazan.sharaya.yes@gmail.com>
2024-03-14 11:50:55 +0300

    Add new line for better docstring formatting

--- xrnn/__init__.py
+++ xrnn/__init__.py
@@ -3,6 +3,7 @@
   train neural networks similar to Keras.
 * xrnn only requires one third library dependency which is NumPy (almost likely you had it already installed).
 * For more information, look at `layers.py` and `models.py` docstrings, and for a minimal working example of how to use
-  the package to train a CNN on MNIST dataset, see `example.py`."""
+  the package to train a CNN on MNIST dataset, see `example.py`.
+"""
 
 __version__ = "1.1.0"
